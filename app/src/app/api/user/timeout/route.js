1438b9399a2f38bd2c8f1dc07414413e00fc58e0
Yakup Altay <yakup.altay@boun.edu.tr>
2025-02-22 01:21:00 +0300

    feat: Integrate Oracle for user timeout functionality

--- app/src/app/api/user/timeout/route.js
+++ app/src/app/api/user/timeout/route.js
@@ -1,9 +1,13 @@
+import { Oracle } from "@chopinframework/next";
+
 import { User } from '../../../../../../app/models/user/User';
 
 export async function POST(req) {
   const body = await req.json();
 
-  User.timeOutUserById(body.id, (err) => {
+  const dateNow = await Oracle.now();
+
+  User.timeOutUserById(body.id, dateNow, (err) => {
     if (err)
       return new Response({ success: false, error: err });
 
