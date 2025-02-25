6f2f95c7eef85acf3efb653b44f2b21eab9264c5
Yakup Altay <yakup.altay@boun.edu.tr>
2025-02-25 16:19:11 +0300

    add user name handling: implement name availability check and purchase functionality

---
+++ app/src/app/api/name/mint/route.js
@@ -0,0 +1,24 @@
+import { User } from '../../../../../models/user/User';
+import { getAddress } from '@chopinframework/next';
+
+import connectDB from '../../../../../lib/db';
+
+export async function POST(req) {
+  const body = await req.json();
+
+  const data = {
+    buyerPublicKey: body.chopin_public_key,
+    name: body.name
+  };
+
+  await connectDB();
+
+  return Response.json(await new Promise((resolve) => {
+    User.purchaseNameByPublicKey(data, (err, name) => {
+      if (err)
+        resolve({ success: false, error: err });
+
+      resolve({ success: true, data: name });
+    });
+  }));
+};
